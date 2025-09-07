f5caec8b6d6daf52f8c016724ca58d4f6a0afaef
Jinyu Xie <xjygr08@gmail.com>
2025-09-06 17:35:23 -0700

    Optimize data loader

--- cs336_basics/data_loader.py
+++ cs336_basics/data_loader.py
@@ -7,7 +7,13 @@ def get_batch(dataset: npt.NDArray, batch_size: int, context_length: int, device
     size = dataset.size
     start_idx = torch.randint(0, size - context_length, size=(batch_size,))
     batch_x = torch.stack([torch.tensor(dataset[i:i + context_length], dtype=torch.int64)
-                          for i in start_idx]).to(device)
+                          for i in start_idx])
     batch_y = torch.stack([torch.tensor(dataset[i + 1: i + context_length + 1], dtype=torch.int64)
-                          for i in start_idx]).to(device)
+                          for i in start_idx])
+    if device == "cuda":
+        batch_x = batch_x.pin_memory().to(device, non_blocking=True)
+        batch_y = batch_y.pin_memory().to(device, non_blocking=True)
+    else:
+        batch_x = batch_x.to(device)
+        batch_y = batch_y.to(device)
     return batch_x, batch_y
